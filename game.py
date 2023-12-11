5c5c789700b942c28db0a6be41e350bbcae63d7e
Dimitri <creativedima7@gmail.com>
2023-12-11 21:55:47 +0100

    Game menu

--- game.py
+++ game.py
@@ -17,3 +17,22 @@ class Game:
         font = pygame.font.SysFont(None, 36)
         text = font.render("Score: " + str(self.score), True, (255, 255, 255))
         screen.blit(text, (10, 10))  # Display the score at (10, 10) on the screen
+
+    def display_menu(self, screen):
+        font = pygame.font.Font(None, 36)
+        WHITE = (255, 255, 255)
+        width, height = screen.get_width(), screen.get_height()
+
+        # Texts for the buttons
+        play_text = font.render("Play", True, WHITE)
+        quit_text = font.render("Quit", True, WHITE)
+
+        # Rectangles for buttons
+        play_rect = play_text.get_rect(center=(width // 2, height // 2 - 50))
+        quit_rect = quit_text.get_rect(center=(width // 2, height // 2 + 50))
+
+        # Display buttons on the screen
+        screen.blit(play_text, play_rect)
+        screen.blit(quit_text, quit_rect)
+
+        return play_rect, quit_rect
--- main.py
+++ main.py
@@ -9,23 +9,56 @@ def run_game():
     WIDTH, HEIGHT = 800, 600
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Spaceship")
-    spaceship = Spaceship(WIDTH, HEIGHT,screen)
+
     #enemy = Enemy(WIDTH, HEIGHT, screen)
     running = True
     clock = pygame.time.Clock()
     game=Game()
     #enemies = [Enemy(WIDTH, HEIGHT, screen) for _ in range(1)]  # Create multiple enemies
     # Inside the main game loop:
-    enemy_wave = EnemyWave(WIDTH, HEIGHT, screen, spaceship, game)  # Pass your screen width and height variables here
     while running:
         screen.fill((0, 0, 0))
 
+        play_rect, quit_rect = game.display_menu(screen)  # Display the main menu
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if play_rect.collidepoint(mouse_pos):
+                    start_game()
+                elif quit_rect.collidepoint(mouse_pos):
+                    running = False
+
+        pygame.display.flip()
+
+
+
+
+    pygame.quit()
+    sys.exit()
+
+
+def start_game():
+    pygame.init()
+    WIDTH, HEIGHT = 800, 600
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Spaceship")
+    spaceship = Spaceship(WIDTH, HEIGHT,screen)
+    running = True
+    clock = pygame.time.Clock()
+    game = Game()  # Initialize the game
+    enemy_wave = EnemyWave(WIDTH, HEIGHT, screen, spaceship, game)  # Pass your screen width and height variables here
+    
+    while running:
+        screen.fill((0, 0, 0))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-
+        # Your game logic goes here...
         spaceship.handle_input()       
         spaceship.draw_health_bar(screen)
         enemy_wave.update()
@@ -38,6 +71,9 @@ def run_game():
         pygame.display.flip()
 
         clock.tick(60)  # Cap the frame rate to 60 FPS
+
+        pygame.display.flip()
+        clock.tick(60)
 
     pygame.quit()
     sys.exit()
